59462e12667b355b876ba247efa656c8f83b0276
TotoroTron <brian.cheng.1997@gmail.com>
2024-06-26 13:37:16 -0400

    working

--- ex4/main.py
+++ ex4/main.py
@@ -2,8 +2,7 @@ from multiprocessing import Pool
 
 import numpy as np
 
-from cvxpy import Minimize, Problem, Variable, Parameter, norm, sum_squares
-
+from cvxpy import Minimize, Problem, Variable, norm, sum_squares
 
 # Problem data.
 m = 100
@@ -15,21 +14,20 @@ gamma = 0.1
 
 NUM_PROCS = 4
 
-
 def prox(args):
     f, v = args
+    x = Variable((n, 1))
     f += (rho/2)*sum_squares(x - v)
     Problem(Minimize(f)).solve()
     return x.value
 
+# Setup problem.
 rho = 1.0
-x = Variable(n, )
+x = Variable((n, 1))
 funcs = [sum_squares(A @ x - b), gamma * norm(x, 1)]
 ui = [np.zeros((n, 1)) for func in funcs]
 xbar = np.zeros((n, 1))
 pool = Pool(NUM_PROCS)
-
-
 # ADMM loop.
 for i in range(50):
     prox_args = [xbar - u for u in ui]
@@ -42,5 +40,3 @@ prob = Problem(Minimize(sum(funcs)))
 result = prob.solve()
 print("ADMM best", (sum_squares(np.dot(A, xbar) - b) + gamma * norm(xbar, 1)).value)
 print("ECOS best", result)
-
-

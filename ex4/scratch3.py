3956f753691661c66702630fa1308e5f34793b69
TotoroTron <brian.cheng.1997@gmail.com>
2024-07-03 17:11:44 -0400

    explore get_problem_data

--- ex4/scratch3.py
+++ ex4/scratch3.py
@@ -1,8 +1,7 @@
 
 from multiprocessing import Pool
 import numpy as np
-from cvxpy import Minimize, Problem, Variable, norm, sum_squares
-
+from cvxpy import Minimize, Problem, Variable, norm, sum_squares, SCS 
 # Problem data.
 m = 100
 n = 75
@@ -48,6 +47,15 @@ for idx, loss in enumerate(list_loss):
 
 print("ADMM best", list_loss[-1])
 print("ECOS best", result)
+
+data, chain, inverse_data = prob.get_problem_data(SCS)
+
+print("DATA:")
+print(data)
+print("CHAIN:")
+print(chain)
+print("INVERSE DATA:")
+print(inverse_data)
 
 pool.close()
 pool.join()
